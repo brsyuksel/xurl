f21c80efc463aa02d00b523fa86f539ee03e60ff
Baris Yuksel <brsyuksel@gmail.com>
2022-08-01 02:11:01 +0200

    resolving http4s prometheus dep

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,18 +2,19 @@ import sbt._
 
 object Dependencies {
   object versions {
-    val cats       = "2.8.0"
-    val catsEffect = "3.3.14"
-    val newtype    = "0.4.4"
-    val refined    = "0.10.1"
-    val derevo     = "0.13.0"
-    val log4cats   = "2.3.2"
-    val skunk      = "0.3.1"
-    val redis      = "1.2.0"
-    val circe      = "0.14.2"
-    val http4s     = "0.23.14"
-    val pureConfig = "0.17.1"
-    val slf4j      = "1.7.36"
+    val cats             = "2.8.0"
+    val catsEffect       = "3.3.14"
+    val newtype          = "0.4.4"
+    val refined          = "0.10.1"
+    val derevo           = "0.13.0"
+    val log4cats         = "2.3.2"
+    val skunk            = "0.3.1"
+    val redis            = "1.2.0"
+    val circe            = "0.14.2"
+    val http4s           = "0.23.14"
+    val http4sPrometheus = "0.24.1"
+    val pureConfig       = "0.17.1"
+    val slf4j            = "1.7.36"
 
     val weaver = "0.7.14"
 
@@ -41,7 +42,7 @@ object Dependencies {
     val http4sEmberServer   = "org.http4s"            %% "http4s-ember-server"       % versions.http4s
     val http4sEmberClient   = "org.http4s"            %% "http4s-ember-client"       % versions.http4s
     val http4sCirce         = "org.http4s"            %% "http4s-circe"              % versions.http4s
-    val http4sPrometheus    = "org.http4s"            %% "http4s-prometheus-metrics" % versions.http4s
+    val http4sPrometheus    = "org.http4s"            %% "http4s-prometheus-metrics" % versions.http4sPrometheus
     val pureConfig          = "com.github.pureconfig" %% "pureconfig"                % versions.pureConfig
 
     val weaverCats       = "com.disneystreaming" %% "weaver-cats"       % versions.weaver
