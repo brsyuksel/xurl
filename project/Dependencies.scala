2bc1bfd90bb9300112899166951e93e84d31ed9d
Scala Steward <scala_steward@virtuslab.com>
2022-12-06 23:39:31 +0000

    Update cats-effect to 3.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object versions {
-    val catsEffect       = "3.4.1"
+    val catsEffect       = "3.4.2"
     val cats             = "2.9.0"
     val newtype          = "0.4.4"
     val refined          = "0.10.1"
