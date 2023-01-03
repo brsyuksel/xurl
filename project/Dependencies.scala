af41c761e0f3fb7cd5279d5eeb04328cea37d191
Scala Steward <scala_steward@virtuslab.com>
2023-01-03 13:29:31 +0000

    Revert commit(s) 5365957

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val log4cats         = "2.5.0"
     val skunk            = "0.3.2"
     val redis            = "1.3.0"
-    val circe            = "0.14.3"
+    val circe            = "0.14.2"
     val http4s           = "0.23.15"
     val http4sPrometheus = "0.24.2"
     val slf4j            = "2.0.6"
