6c207da819d9af294e51eece2e93c94a2b6436f4
Scala Steward <scala_steward@virtuslab.com>
2023-01-19 15:05:59 +0000

    Update skunk-core to 0.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val refined          = "0.10.1"
     val derevo           = "0.13.0"
     val log4cats         = "2.5.0"
-    val skunk            = "0.3.2"
+    val skunk            = "0.5.0"
     val redis            = "1.3.0"
     val circe            = "0.14.2"
     val http4s           = "0.23.15"
