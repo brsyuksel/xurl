7539d211dceb3cdbf41b0cf5cf56b2c17dfd96a5
Scala Steward <scala_steward@virtuslab.com>
2023-01-23 13:48:17 +0000

    Update redis4cats-effects, ... to 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val derevo           = "0.13.0"
     val log4cats         = "2.5.0"
     val skunk            = "0.3.2"
-    val redis            = "1.3.0"
+    val redis            = "1.4.0"
     val circe            = "0.14.2"
     val http4s           = "0.23.15"
     val http4sPrometheus = "0.24.2"
