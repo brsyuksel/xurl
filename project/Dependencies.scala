f74de06722bb9497dac07b9c511112a09fe57797
Scala Steward <scala_steward@virtuslab.com>
2024-09-14 13:38:49 +0000

    Update http4s-prometheus-metrics to 0.25.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val circe            = "0.14.9"
     val redis            = "1.7.1"
     val http4s           = "0.23.27"
-    val http4sPrometheus = "0.24.7"
+    val http4sPrometheus = "0.25.0"
     val slf4j            = "2.0.16"
     val pureConfig       = "0.17.7"
 
